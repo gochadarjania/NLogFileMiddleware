00697e4cecb55d058ba00d4e5ddb5eab515a4883
Gocha Darjania <gocha.darjania@procreditbank.ge>
2022-10-24 18:24:01 +0400

    first commit

---
+++ NLogFile/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLogFile.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hello, this is the index!");
+            return View();
+        }
+    }
+}
---
+++ NLogFile/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLogFile.Middleware
+{
+    public class LoggerMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        public LoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LoggerMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            try
+            {
+                _logger.LogError("Performing file logging in Middleware operation");
+
+                // Perform some Database action into Middleware 
+
+
+                _logger.LogWarning("Performing Middleware Save operation");
+
+                //Save Data
+
+
+                _logger.LogInformation("Save Comepleted");
+
+                await _next(context);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex.Message}");
+            }
+        }
+    }
+}
